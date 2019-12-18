6b15384e4a18d397ca22727764b89066bb1b2e80
ms <ms@limbozz.com>
2019-12-18 14:28:13 +0100

    Allow testing for any appid on android,removed unnecessary log messages.

--- app-installed-checker.js
+++ app-installed-checker.js
@@ -21,11 +21,9 @@ class AppInstalledChecker {
             Linking
                 .canOpenURL(proto + '://' + query || '')
                 .then((isInstalled) => {
-                    console.log('isInstalled', isInstalled);
                     resolve(isInstalled);
                 })
                 .catch((err) => {
-                    console.log('erroer', err);
                     reject(err);
                 });
         });
@@ -39,7 +37,7 @@ class AppInstalledChecker {
     }
 
     static isAppInstalledAndroid(key) {
-        return this.checkPackageName(APP_LIST[key] != null ? APP_LIST[key].pkgName : key);
+        return this.checkPackageName(key);
     }
 
     static isAppInstalledIOS(key) {
